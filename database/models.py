2294300c8116bbc39b387750ba61b18456c5c449
ejaszczuk <emj1054@gmail.com>
2021-02-16 16:42:22 +0100

    Add basic User CRUD

--- database/models.py
+++ database/models.py
@@ -11,14 +11,33 @@ class User(db.Model):
     password = db.Column(db.String(40), nullable=False)
     firstname = db.Column(db.String(20), nullable=False)
     surname = db.Column(db.String(40), nullable=False)
+    sex = db.Column(db.Integer, nullable=False)
+    active = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False,
                            default=db.func.current_timestamp())
-    is_admin = db.Column(db.Boolean, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False,
+                           default=db.func.current_timestamp())
 
-    def __init__(self, email, password, firstname, surname, created_at, is_admin):
+    def __init__(self, email, password, firstname, surname, sex, active, created_at, updated_at):
         self.email = email
         self.password = password
         self.firstname = firstname
         self.surname = surname
+        self.sex = sex
+        self.active = active
         self.created_at = created_at
-        self.is_admin = is_admin
+        self.updated_at = updated_at
+
+
+class Institution(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45), nullable=False)
+    city = db.Column(db.String(45), nullable=False)
+    address = db.Column(db.String(128), nullable=False)
+    contact_number = db.Column(db.String(15), nullable=False)
+
+    def __init__(self, name, city, address, contact_number):
+        self.name = name
+        self.city = city
+        self.address = address
+        self.contact_number = contact_number
